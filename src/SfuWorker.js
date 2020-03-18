ecea3620083117cd959b47aa6db0143ff5ddb8a7
Anthony obeta <choobey21@gmail.com>
2020-03-18 14:41:32 -0700

    able to connect to room, create routers

---
+++ src/SfuWorker.js
@@ -0,0 +1,45 @@
+const mediasoup = require('mediasoup');
+
+let _worker;
+let _routers = new Map();
+let _mediaCodecs = mediasoup.getSupportedRtpCapabilities().codecs.filter(
+  // have to filter out codecs the other two 'audio/CN' codecs to avoid duplicate prefferredPayloadTypes error
+  codec =>
+    codec.mimeType === 'audio/CN' &&
+    codec.clockRate !== 16000 &&
+    codec.clockRate !== 8000,
+);
+
+async function createWorker() {
+  _worker = await mediasoup.createWorker({
+    logLevel: 'debug',
+  });
+  return _worker;
+}
+
+async function createNewRouter() {
+  // create worker if it hasnt been created already.
+  if (!_worker) await createWorker();
+
+  const router = await _worker.createRouter({ _mediaCodecs });
+  _routers.set(router.id, router);
+  return router;
+}
+
+function getRouter(routerId) {
+  return _routers.get(routerId);
+}
+
+function getRouters() {
+  return _routers;
+}
+
+function getWorker() {
+  return _worker;
+}
+
+module.exports = {
+  createWorker,
+  getWorker,
+  createNewRouter,
+};
--- src/socketServer.js
+++ src/socketServer.js
@@ -1,21 +1,71 @@
 const socketIO = require('socket.io');
+const { createNewRouter } = require('./SfuWorker');
 
-const _rooms = [];
+let _socketServer;
+const _rooms = new Map();
 
 function startSocketServer(webServer) {
-  socketServer = new socketIO(webServer, {
+  _socketServer = new socketIO(webServer, {
     log: true,
     serveClient: false,
   });
 
-  socketServer.on('connection', handleClientConnect);
+  _socketServer.on('connection', handleClientConnect);
 }
 
 function handleClientConnect(socket) {
   console.log('new client connected: ', socket.id);
-  socket.on('joinRoom', room => {
-    socket.join(room);
+
+  socket.on('joinRoom', async (roomId, callback) => {
+    console.log('user wants to connect with specific room --> ', roomId);
+    // join room so that we can communicate with other people in the room.
+    try {
+      await new Promise(r => socket.join(roomId, r));
+      await createOrJoinRoom(roomId, socket.id);
+      callback({ success: true });
+    } catch (e) {
+      callback({ success: false, error: e.message });
+    }
   });
+}
+
+async function createOrJoinRoom(roomId, participantId) {
+  if (_rooms.has(roomId)) {
+    // room already exists, add participant to the room
+    const room = _rooms.get(roomId);
+    room.participants.push(new Participant({ participantId }));
+    return room;
+  } else {
+    // room doesnt exist. create a new room, with a new participant in it.
+    const router = await createNewRouter();
+    const room = new Room({
+      router,
+      roomId,
+      initialParticipantId: participantId,
+    });
+    // save the room for later reference
+    _rooms.set(roomId, room);
+    return room;
+  }
+}
+
+class Room {
+  constructor({ router, roomId, initialParticipantId }) {
+    this.id = roomId;
+    this.router = router;
+    this.participants = [
+      new Participant({ participantId: initialParticipantId }),
+    ];
+  }
+}
+
+class Participant {
+  constructor({ participantId }) {
+    this.id = participantId;
+    this.transports = [];
+    this.producers = [];
+    this.consumers = [];
+  }
 }
 
 module.exports = startSocketServer;
